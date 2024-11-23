6fe6554f5eb30ac00dc9ce46d9ad649cb8e6740e
Sergio Carrillo <sergiocarrillo_@outlook.com>
2024-11-23 04:43:34 -0400

    Hoffman funciona

---
+++ testHoffman.hs
@@ -0,0 +1,31 @@
+import FrecuenciaPlus
+import Hoffman
+import qualified Data.Map as Map
+
+-- Función para crear un árbol de Hoffman más grande
+crearArbol :: Hoffman
+crearArbol = 
+    fusionHoffman 
+    (fusionHoffman (nuevoHoffman 'a') (nuevoHoffman 'b')) 
+    (fusionHoffman (nuevoHoffman 'c') (fusionHoffman (nuevoHoffman 'd') (nuevoHoffman 'e')))
+
+main :: IO ()
+main = do
+ 
+  -- Crear un árbol grande
+  let tree = crearArbol
+
+  -- Mostrar la codificación de Hoffman para todos los caracteres en el árbol
+  let cod = codificacion tree
+  putStrLn "Codificación de Hoffman:"
+  print cod
+
+  -- Ejemplo de obtener el carácter de una hoja
+  putStrLn "\nCaracter de la hoja 'a':"
+  print $ obtenerCaracter (nuevoHoffman 'a')
+
+  -- Ejemplo de los subárboles
+  putStrLn "\nSubárbol izquierdo de la rama principal:"
+  print $ arbolIzquierdo tree
+  putStrLn "Subárbol derecho de la rama principal:"
+  print $ arbolDerecho tree
