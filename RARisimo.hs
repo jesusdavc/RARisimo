69195e4ee8ecedfb7886c9e8f3cbf09f22d89ae6
Sergio Carrillo <sergiocarrillo_@outlook.com>
2024-11-23 13:59:33 -0400

    funciones ganadores, hoffman, rarisimo del modulo3

--- RARisimo.hs
+++ RARisimo.hs
@@ -1,24 +1,14 @@
 module RARisimo
     ( frecuencias,
-      -- ganadores,
-      -- hoffman,
-      -- rarisimo
+      ganadores,
+      hoffman,
+      rarisimo
     ) where
 
 import Hoffman
 import FrecuenciaV2
 import qualified Data.Map as Map
-import Data.List (nub)
-
--- frecuencias :: String -> [Frecuencia Hoffman]
--- frecuencias str = map (\(c, n) -> Frecuencia (nuevoHoffman c) n) freqList
---   where
---     freqList = countChars str
-
---     countChars :: String -> [(Char, Int)]
---     countChars [] = []
---     countChars (x:xs) = (x, length (filter (== x) (x:xs))) : countChars (filter (/= x) xs)
-
+import Data.List (nub, sortOn)
 
 frecuencias :: String -> [Frecuencia Hoffman]
 frecuencias str = map (\c -> Frecuencia (nuevoHoffman c) (count c str)) (nub str)
@@ -27,30 +17,32 @@ frecuencias str = map (\c -> Frecuencia (nuevoHoffman c) (count c str)) (nub str
     count x = length . filter (== x)
 
 
+ganadores :: [Frecuencia a] -> Maybe (Frecuencia a, Frecuencia a, [Frecuencia a])
+ganadores [] = Nothing
+ganadores [_] = Nothing
+ganadores xs = Just (x, y, resto)
+  where
+    sorted = sortOn frecuencia xs
+    x = head sorted
+    y = sorted !! 1
+    resto = drop 2 sorted
 
--- ganadores :: [Frecuencia a] -> Maybe (Frecuencia a, Frecuencia a, [Frecuencia a])
--- ganadores [] = Nothing
--- ganadores [x] = Nothing
--- ganadores (x:y:xs) = Just (x, y, xs')
---   where
---     sorted = sortOn frecuencia (x:y:xs)
---     (x':y':xs') = sorted
---     (x, y) = (x', y')
 
--- hoffman :: String -> Maybe Hoffman
--- hoffman "" = Nothing
--- hoffman str = Just $ construirHoffman (frecuencias str)
---   where
---     construirHoffman :: [Frecuencia Hoffman] -> Hoffman
---     construirHoffman [x] = valor x
---     construirHoffman xs = construirHoffman (fusionarLosMenores xs)
+hoffman :: String -> Maybe Hoffman
+hoffman "" = Nothing
+hoffman str = Just $ construirHoffman (frecuencias str)
+  where
+    construirHoffman :: [Frecuencia Hoffman] -> Hoffman
+    construirHoffman [x] = valor x
+    construirHoffman xs = construirHoffman (fusionarLosMenores xs)
 
---     fusionarLosMenores :: [Frecuencia Hoffman] -> [Frecuencia Hoffman]
---     fusionarLosMenores (x:y:xs) = (Frecuencia (fusionHoffman (valor x) (valor y)) (frecuencia x + frecuencia y)) : xs
---     fusionarLosMenores _ = error "La lista no tiene suficientes elementos para fusionar."
+    fusionarLosMenores :: [Frecuencia Hoffman] -> [Frecuencia Hoffman]
+    fusionarLosMenores (x:y:xs) = (Frecuencia (fusionHoffman (valor x) (valor y)) (frecuencia x + frecuencia y)) : xs
+    fusionarLosMenores _ = error "La lista no tiene suficientes elementos para fusionar."
 
--- rarisimo :: String -> Map.Map Char String
--- rarisimo str = codificacion (fromJust (hoffman str))
---   where
---     fromJust (Just x) = x
---     fromJust Nothing = error "No se pudo construir el árbol de Hoffman"
+
+rarisimo :: String -> Map.Map Char String
+rarisimo str = codificacion (fromJust (hoffman str))
+  where
+    fromJust (Just x) = x
+    fromJust Nothing = error "No se pudo construir el árbol de Hoffman"
--- testModulo3.hs
+++ testModulo3.hs
@@ -1,5 +1,8 @@
 import RARisimo
+import FrecuenciaV2
+import Hoffman
 import qualified Data.Map as Map
+
 
 main :: IO ()
 main = do
@@ -8,3 +11,15 @@ main = do
     -- Frecuencias de los caracteres
     let freqs = frecuencias str
     print freqs
+
+    -- Los dos ganadores con menor frecuencia
+    let g = ganadores freqs
+    print g
+
+    -- Construcción del árbol de Huffman
+    let tree = hoffman str
+    print tree
+
+    -- Codificación de Huffman
+    let cod = rarisimo str
+    print cod
