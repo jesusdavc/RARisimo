2b7379722847259cd53397787107ecfd71521f17
Jesús David Cuéllar <jedave.co@gmail.com>
2024-11-22 22:51:41 -0400

    Funciones de acceso

--- Hoffman.hs
+++ Hoffman.hs
@@ -1,7 +1,8 @@
+--Funciones de construcción
 -- Definición del tipo de datos Hoffman
 data Hoffman = Hoja Char
-             | Nodo Hoffman Hoffman
-             deriving (Show, Eq)
+            | Rama Hoffman Hoffman
+            deriving (Show, Eq)
 
 -- Función para crear un árbol Hoffman con una sola hoja
 nuevoHoffman :: Char -> Hoffman
@@ -9,5 +10,20 @@ nuevoHoffman = Hoja
 
 -- Función para combinar dos árboles Hoffman
 fusionHoffman :: Hoffman -> Hoffman -> Hoffman
-fusionHoffman = Nodo
+fusionHoffman = Rama
 
+--Funciones de acceso
+-- Devuelve el carácter de una hoja
+obtenerCaracter :: Hoffman -> Char
+obtenerCaracter (Hoja c) = c
+obtenerCaracter _ = error "El árbol no es una hoja"
+
+-- Devuelve el subárbol izquierdo
+arbolIzquierdo :: Hoffman -> Hoffman
+arbolIzquierdo (Rama izq _) = izq
+arbolIzquierdo _ = error "El árbol no es una rama"
+
+-- Devuelve el subárbol derecho
+arbolDerecho :: Hoffman -> Hoffman
+arbolDerecho (Rama _ der) = der
+arbolDerecho _ = error "El árbol no es una rama"
